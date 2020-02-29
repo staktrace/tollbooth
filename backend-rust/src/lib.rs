28f0bcf20936ce337484453e7c8ae9b6898c11fc
Kartikaya Gupta <kats@trevize.staktrace.com>
2020-02-29 06:28:37 -0500

    Tokio-ize the backend

--- backend-rust/src/lib.rs
+++ backend-rust/src/lib.rs
@@ -1,5 +1,7 @@
-use std::thread::{JoinHandle, spawn};
-use std::sync::mpsc::{channel, Receiver, Sender};
+extern crate tokio;
+
+use tokio::task::JoinHandle;
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 
 #[derive(Debug, Clone)]
 pub enum Wallet {
@@ -14,7 +16,7 @@ enum ListenerCommand {
 struct WalletListener {
     listen_thread: JoinHandle<()>,
     wallet: Wallet,
-    tx: Sender<ListenerCommand>,
+    tx: UnboundedSender<ListenerCommand>,
 }
 
 pub struct Tollbooth {
@@ -26,9 +28,9 @@ impl Tollbooth {
         let mut listeners = Vec::new();
         for wallet in wallets.into_iter() {
             let wallet_clone = wallet.clone();
-            let (tx, rx) = channel();
-            let listen_thread = spawn(move || {
-                listen_loop(wallet_clone, rx);
+            let (tx, rx) = unbounded_channel();
+            let listen_thread = tokio::spawn(async move {
+                listen_loop(wallet_clone, rx).await;
             });
             listeners.push(WalletListener {
                 listen_thread,
@@ -41,11 +43,11 @@ impl Tollbooth {
         }
     }
 
-    pub fn shutdown(self) {
+    pub async fn shutdown(self) {
         for listener in self.listeners {
             match listener.tx.send(ListenerCommand::Shutdown) {
                 Ok(_) => {
-                    match listener.listen_thread.join() {
+                    match listener.listen_thread.await {
                         Ok(_) => (),
                         Err(e) => eprintln!("Warning: listener thread for wallet {:?} panicked with {:?}", listener.wallet, e),
                     };
@@ -59,5 +61,9 @@ impl Tollbooth {
     }
 }
 
-fn listen_loop(_wallet: Wallet, _rx: Receiver<ListenerCommand>) {
+async fn listen_loop(_wallet: Wallet, mut rx: UnboundedReceiver<ListenerCommand>) {
+    match rx.recv().await {
+        Some(ListenerCommand::Shutdown) => (),
+        None => eprintln!("Listener task seems to have been detached by dropped parent"),
+    }
 }

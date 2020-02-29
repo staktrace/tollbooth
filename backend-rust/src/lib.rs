b061ab7fa0dbd0639bf4d41714deeb552c32e510
Kartikaya Gupta <kats@trevize.staktrace.com>
2020-02-29 06:59:03 -0500

    Compiling WIP

--- backend-rust/src/lib.rs
+++ backend-rust/src/lib.rs
@@ -1,5 +1,7 @@
 extern crate tokio;
+extern crate reqwest;
 
+use futures_util::stream::StreamExt;
 use tokio::task::JoinHandle;
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 
@@ -61,9 +63,56 @@ impl Tollbooth {
     }
 }
 
-async fn listen_loop(_wallet: Wallet, mut rx: UnboundedReceiver<ListenerCommand>) {
-    match rx.recv().await {
-        Some(ListenerCommand::Shutdown) => (),
-        None => eprintln!("Listener task seems to have been detached by dropped parent"),
+async fn listen_loop(wallet: Wallet, mut rx: UnboundedReceiver<ListenerCommand>) {
+    let url = match &wallet {
+        Wallet::StellarTest(pubkey) => format!("https://horizon-testnet.stellar.org/accounts/{}/payments", pubkey),
+        Wallet::Stellar(pubkey) => format!("https://horizon.stellar.org/accounts/{}/payments", pubkey),
+    };
+    let req = reqwest::Client::new()
+            .get(&url)
+            .header("Accept", "text/event-stream")
+            .send()
+            .await
+            .and_then(|r| r.error_for_status());
+    let mut stream = match req {
+        Err(e) => {
+            eprintln!("Error on initial request for wallet {:?}: {:?}", &wallet, e);
+            return;
+        }
+        Ok(r) => r.bytes_stream(),
+    };
+
+    let mut shutdown = false;
+    while !shutdown {
+        tokio::select! {
+            msg = rx.recv() => {
+                match msg {
+                    None => {
+                        eprintln!("Listener task seems to have been detached by dropped parent");
+                        shutdown = true;
+                    }
+                    Some(ListenerCommand::Shutdown) => {
+                        shutdown = true;
+                    }
+                }
+            }
+            item = stream.next() => {
+                match item {
+                    None => {
+                        eprintln!("Stream termination for wallet {:?}", &wallet);
+                        // TODO: restart with new request
+                        shutdown = true;
+                    }
+                    Some(Err(e)) => {
+                        eprintln!("Error while streaming payments for wallet {:?}: {:?}", &wallet, e);
+                        // TODO: restart with new request
+                        shutdown = true;
+                    }
+                    Some(Ok(bytes)) => {
+                        println!("Chunk: {:?}", bytes);
+                    }
+                }
+            }
+        }
     }
 }
